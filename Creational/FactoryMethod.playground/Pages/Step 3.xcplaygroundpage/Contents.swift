18a755140e28fea373450a504264b22e2abe50b4
Jeremy  Bailly <jeremy.bailly@hotmail.com>
2024-10-01 17:43:09 +0200

    First steps

---
+++ Creational/FactoryMethod.playground/Pages/Step 3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,84 @@
+
+// MARK: - Models
+
+protocol Music {
+    func play()
+}
+
+// MARK: RockMusic
+
+struct RockMusic: Music {
+    
+    func play() {
+        print("Play rock music 🎸")
+    }
+    
+}
+
+struct ClassicMusic: Music {
+    
+    func play() {
+        print("Play classic music 🎸")
+    }
+    
+}
+
+struct PopMusic: Music {
+    
+    func play() {
+        print("Play classic music 🎸")
+    }
+    
+}
+// MARK: - Music Factory
+
+struct MusicFactory {
+    
+    // MARK: Public method
+    
+    func createMusic(type: MusicType) -> Music {
+        switch type {
+        case .rock:
+            return RockMusic()
+        case .classic:
+            return ClassicMusic()
+        case .pop:
+            return PopMusic()
+        }
+    }
+}
+
+// MARK: - MusicType Enum
+
+enum MusicType {
+    case rock
+    case classic
+    case pop
+}
+
+// MARK: - App
+
+struct MusicApp {
+    
+    // MARK: Private property
+    
+    private let music: Music
+    
+    // MARK: - Initializer
+    
+    init(factory: MusicFactory, musicType: MusicType) {
+        music = factory.createMusic(type: musicType)
+    }
+    
+    // MARK: - Public method
+    
+    func startMusic() {
+        music.play()
+    }
+}
+
+// MARK: - Result
+
+let musicFactory = MusicFactory()
+let musicApp = MusicApp(factory: musicFactory, musicType: .classic)
+musicApp.startMusic()
